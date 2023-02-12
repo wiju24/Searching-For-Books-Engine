2794ad8a147c336303f9d53ea1377c73c7050a11
Vijithiran Navajeevayokan <vijithiran_navajeevayokan@hotmail.com>
2023-02-11 20:07:16 -0500

    Finished the resolvers.js file for mutations and query

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -3,5 +3,71 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    
-}
+    Query: {
+        me: async (parent, args, context) => {
+            if(context.user) {
+                const userData = await User
+                .findOne({_id: context.user._id})
+                .select('-__v -password');
+                return userData;
+            }
+            throw new AuthenticationError('You still need to log in!');
+        }
+    },
+
+    Mutation: {
+
+        login: async (parent, {email, password}) => {
+            const user = await User
+            .findOne({email});
+
+                if (!user) {
+                    throw new AuthenticationError('The Username is incorrect')
+                }
+            
+            const password = await password
+            .isCorrectPassword(password);
+
+                if (!password) {
+                    throw new AuthenticationError('The password is incorrect')
+                }
+
+            const token = signToken (user);
+            return {token, user};
+        },
+
+        insertUser: async(parent, args) => {
+            const user = await User.create(args);
+            const token = signToken (user);
+            return {token, user};
+        },
+
+        saveBook: async (parent, {bookSearch}, context) => {
+            if (context.user) {
+                const bookSaved = await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {savedBooks: bookSearch}},
+                    {new: true}
+                );
+                return bookSaved;
+            }
+            throw new AuthenticationError('Must be logged in to move forward');
+        },
+
+        removeBook: async (parent, args, context) => {
+            if(context.user) {
+                const bookRemoved = await User.findByIdAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks: {bookId:args.bookId}}},
+                    {new: true}
+                );
+
+                return bookRemoved;
+            }
+            
+            throw new AuthenticationError('Must be logged in to move forward');
+        }
+    }
+};
+
+module.exports = resolvers;

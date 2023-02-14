96ebb6b90478185306fc341b72d0057dca0fff22
Vijithiran Navajeevayokan <vijithiran_navajeevayokan@hotmail.com>
2023-02-13 21:02:56 -0500

    Resolved the issue where the client won't load the screen for searching books.

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,57 @@
 import React from 'react';
+
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
-
-import {ApolloClient, ApolloProvider, InMemoryCache, createHttpLink} from '@apollo/client';
-import {setContext} from '@apollo/client/link/context';
-import Switch from 'react-bootstrap/esm/Switch';
-
-
-const httpLink = createHttpLink ({
+const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-const authenticationLink = setContext (( _, {headers}) =>{
+const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : '',
-  },
-};
+    },
+  };
 });
 
-const client = new ApolloClient ({
-  link: authenticationLink.concat(httpLink),
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-})
-
+});
 
 function App() {
   return (
-  <ApolloProvider client = {client}>
-    <Router>
+    <ApolloProvider client={client}>    <Router>
       <>
         <Navbar />
-        <Switch>
-          
-          <Route exact path ='/' component = {SearchBooks} />
-          <Route exact path ='/saved' component = {SavedBooks} />
-          <Route render = {() => <h1 className='display-2'>Incorrect Page!</h1>} />
-
-        </Switch>
+        <Routes>
+          <Route 
+            path='/' 
+            element={<SearchBooks />} 
+          />
+          <Route 
+            path='/saved' 
+            element={<SavedBooks />} 
+          />
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Incorrect Page!</h1>}
+          />
+        </Routes>
       </>
     </Router>
-  </ApolloProvider>
+    </ApolloProvider>
+
   );
 }
 
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,70 +1,70 @@
-const { User } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        me: async (parent, args, context) => {
-            if(context.user) {
-                const userData = await User.findOne({_id: context.user._id})
-                .select('-__v -password');
-                return userData;
-            }
-            throw new AuthenticationError('You still need to log in!');
-        }
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+        return userData;
+      }
+
+      throw new AuthenticationError('Not logged in');
     },
+  },
 
-    Mutation: {
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-        login: async (parent, {email, password}) => {
-            const user = await User.findOne({email});
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-                if (!user) {
-                    throw new AuthenticationError('The Username is incorrect')
-                }
-            
-            const pass = await user.isCorrectPassword(password);
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
-                if (!pass) {
-                    throw new AuthenticationError('The password is incorrect')
-                }
+      const correctPw = await user.isCorrectPassword(password);
 
-            const token = signToken (user);
-            return {token, user};
-        },
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
 
-        insertUser: async(parent, args) => {
-            const user = await User.create(args);
-            const token = signToken (user);
-            return {token, user};
-        },
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { bookData }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
 
-        saveBook: async (parent, {bookSearch}, context) => {
-            if (context.user) {
-                const bookSaved = await User.findByIdAndUpdate(
-                    {_id: context.user._id},
-                    {$addToSet: {savedBooks: bookSearch}},
-                    {new: true}
-                );
-                return bookSaved;
-            }
-            throw new AuthenticationError('Must be logged in to move forward');
-        },
+        return updatedUser;
+      }
 
-        removeBook: async (parent, args, context) => {
-            if(context.user) {
-                const bookRemoved = await User.findByIdAndUpdate(
-                    {_id: context.user._id},
-                    {$pull: {savedBooks: {bookId:args.bookId}}},
-                    {new: true}
-                );
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
 
-                return bookRemoved;
-            }
-            
-            throw new AuthenticationError('Must be logged in to move forward');
-        }
-    }
+        return updatedUser;
+      }
+
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
 };
 
 module.exports = resolvers;
